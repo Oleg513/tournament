4ae7e3aff2de5f551c4ac433843f6cf7dc0322be
Олег <74383033+Oleg513@users.noreply.github.com>
2024-04-22 21:08:06 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,9 @@
+// Отримати кнопку та меню
+const toggleMenuButton = document.getElementById('toggleMenuButton');
+const mainMenu = document.getElementById('mainMenu');
 
+// Додати обробник події для кнопки
+toggleMenuButton.addEventListener('click', function() {
+    // Змінити клас, щоб показати/сховати меню
+    mainMenu.classList.toggle('active');
+});
